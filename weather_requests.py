dd149c6f733c0e764ab0068cfb66e4c446c6d996
Szuumii <jakub.szumskii@gmail.com>
2021-04-10 01:25:09 +0200

    Weather parameters extraction

--- weather_requests.py
+++ weather_requests.py
@@ -3,27 +3,35 @@ import json
 
 # API DOC: https://openweathermap.org/api/one-call-api
 
-parameters = {
-    "lat": 52.229676,
-    "lon": 21.012229,
-    "appid": "",
-    "exclude": ["minutely", "daily", "alerts"],
-    "units": "metric"
-}
-
 
 def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
 
-if __name__ == "__main__":
+def get_weather_parameters():
+    parameters = {
+        "lat": 52.229676,
+        "lon": 21.012229,
+        "appid": "fa7600fc40a1fff635b882e00bb122b0",
+        "exclude": ["minutely", "daily", "alerts"],
+        "units": "metric"
+    }
 
     response = requests.get(
         "http://api.openweathermap.org/data/2.5/onecall", params=parameters)
 
-    print(response.status_code)
+    hourly_weather = response.json()["hourly"]
 
-    # jprint(response.json()["hourly"])
+    weather_props = [{"time": sample["dt"], "temperature": sample["temp"],
+                      "clouds": sample["clouds"]} for sample in hourly_weather]
 
-    # print(response.json()["hourly"][0])
+    return weather_props
+
+
+if __name__ == "__main__":
+
+    weather_props = get_weather_parameters()
+
+    for elem in weather_props:
+        print(elem)
