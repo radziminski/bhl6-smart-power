1b7806a8279fb210fdd7a64c1b29d2529561dcd5
Szuumii <jakub.szumskii@gmail.com>
2021-04-09 23:11:41 +0200

    Mock data fill

--- power_computation/other_devices_power_consumption.py
+++ power_computation/other_devices_power_consumption.py
@@ -1,28 +1,36 @@
 import datetime
 
+work_day = {"0": 0.5, "1": 0.5, "2": 0.5, "3": 0.5,
+            "4": 0.5, "5": 3, "6": 3, "7": 3, "8": 2,
+            "9": 2, "9": 2, "10": 1, "10": 1, "11": 1,
+            "12": 1, "13": 1, "14": 1, "15": 1, "16": 2,
+            "17": 2, "18": 2, "19": 2, "20": 1, "21": 1,
+            "22": 1, "23": 1}
+
+free_day = {"0": 0.5, "1": 0.5, "2": 0.5, "3": 0.5,
+            "4": 0.5, "5": 0.5, "6": 0.5, "7": 0.5, "8": 3,
+            "9": 3, "9": 3, "10": 3, "10": 3, "11": 3,
+            "12": 1, "13": 1, "14": 1, "15": 1, "16": 1,
+            "17": 2, "18": 2, "19": 2, "20": 2, "21": 2,
+            "22": 2, "23": 2}
+
+
+value_table = [work_day, work_day, work_day,
+               work_day, work_day, free_day, free_day]
+
 
 def energy_of_other_devices(start_time, end_time):
     time_diff = end_time - start_time
 
     SECONDS_IN_HOUR = 3600
-
     time_diff /= SECONDS_IN_HOUR
-
-    # value_table[day of the week]["time_of_day"] = value_of_energy
-
-    value_table = []
 
     retv = 0
 
-    # value_table[0-4]["0" - "4"] = value for time
-    # etc.....
-    # freedays[5-6]["0" - "7"] = value for time
-
-    # same day check for time range
     for delta_time in range(time_diff.seconds):
-        future_time = datetime.timedelta(hours=start_time.hour + delta_time)
+        future_time = datetime.timedelta(hours=delta_time)
         future_time_and_date = start_time + future_time
-        retv += value[future_time_and_date.weekday()
-                      ][str(future_time_and_date.hour)]
+        retv += value_table[future_time_and_date.weekday()
+                            ][str(future_time_and_date.hour)]
 
     return retv
