6180930ac0cb43ec401e886793d73f7d4d31b6d1
Wiktor Łazarski <wjlazarski@gmail.com>
2021-04-10 10:31:30 +0200

    additional endpoints

--- source/rest_api.py
+++ source/rest_api.py
@@ -1,10 +1,15 @@
+import datetime
+
 import flask
+
+import device_mocks as dmocks
 import optimization_service as svc
+import weather_requests as wapi
 
 app = flask.Flask(__name__)
 
 @app.route("/plan_next_sequence")
-def sys_plan_next_sequence() -> None:
+def get_next_sequence_plan() -> None:
     best_modes_seq, alg_run_params = svc.plan_next_sequence()
 
     merge_info = {
@@ -13,3 +18,21 @@ def sys_plan_next_sequence() -> None:
     }
 
     return flask.jsonify(merge_info)
+
+@app.route("/water_level")
+def get_water_level() -> None:
+    water_mock = dmocks.WaterBank(max_capacity=150)
+
+    level = water_mock.get_water_level(datetime.datetime.now())
+
+    return flask.jsonify({"water_level": level})
+
+@app.route("/accumulator")
+def current_accumulator_power() -> None:
+    pass
+
+@app.route("/weather_forecast")
+def get_weather_forecast() -> None:
+    response = wapi.get_weather_parameters()
+
+    return flask.jsonify(response)
